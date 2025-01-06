489ca8291d80ab6da9c3aaa7e9b12a865c5f8de1
venuchilukala <venuchilukala07@gmail.com>
2025-01-06 19:20:00 +0530

    store changed

--- src/pages/shop/Store.jsx
+++ src/pages/shop/Store.jsx
@@ -4,26 +4,29 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const Store = () => {
   const [stores, setStores] = useState([]);
-  const axiosSecure = useAxiosSecure()
+  const axiosSecure = useAxiosSecure();
 
   useEffect(() => {
     const getStoresData = async () => {
-      const response = await axiosSecure("https://mega-mart-server.onrender.com/stores");
-      const data = await response.json();
-      console.log(data);
-      setStores(data)
+      try {
+        const response = await axiosSecure(
+          "https://mega-mart-server.onrender.com/stores"
+        );
+        console.log("Stores fetched successfully:", response.data);
+        setStores(response.data);
+      } catch (error) {
+        console.error("Failed to fetch stores data:", error);
+        // Optionally, show an alert or display a fallback message
+      }
     };
-    getStoresData()
-  },[]);
+  }, []);
 
   return (
     <div>
       <h1>Store Filter Section</h1>
-      {
-        stores.map((store, index) => (
-          <StoreCard key={index} store={store}/>
-        ))
-      }
+      {stores.map((store, index) => (
+        <StoreCard key={index} store={store} />
+      ))}
     </div>
   );
 };
